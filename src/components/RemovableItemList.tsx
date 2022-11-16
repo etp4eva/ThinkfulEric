73cafcac23cf5b496048549c23d20460b1688567
etp4eva <eric@ericwebsite.info>
2022-11-16 10:47:17 +0100

    Refactor on RemovableItemList

--- src/components/RemovableItemList.tsx
+++ src/components/RemovableItemList.tsx
@@ -1,35 +1,36 @@
-import { useState } from "react"
 import { Text, Button, FlatList, ListRenderItem, View } from "react-native"
 
 type Params = {
-    removeFn: (index: number) => void;
-    data: RemovableListItemInterface[];
+    data: any[];
+    removeFn: (idx: number) => void;
+    minItems?: number;
 }
 
 export interface RemovableListItemInterface {
     label: String;
-    removeFn: () => void;
-}
-
-export const RemovableListItem = (params: RemovableListItemInterface) => {
-    return (
-        <View>
-            <Text>{params.label}</Text>
-            <Button
-                title="X"
-                onPress={params.removeFn}
-            />
-        </View>
-    )
 }
 
 export const RemoveableItemList = (params: Params) => {
+    const isRemoveRendered = (!params.minItems) || params.minItems < params.data.length;
+    
     const renderItem = ({item, index}: {item: RemovableListItemInterface, index: number}) => {
+        let removeButton;
+
+        if (isRemoveRendered)
+        {
+            removeButton = (
+                <Button
+                    title="X"
+                    onPress={() => params.removeFn(index)}
+                />
+            )
+        }
+
         return (
-            <RemovableListItem 
-                label={item.label} 
-                removeFn={() => params.removeFn(index)}
-            />
+            <View>
+                <Text>{item.label}</Text>
+                {removeButton}
+            </View>
         )
     }
 
